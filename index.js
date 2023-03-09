8a479dd0b16488dae8a216ab82efd1725012b766
nicole <nicolenamayan133@gmail.com>
2023-03-09 13:43:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const name = "Joe";
-const height = 74;
+const name = "susan";
+const height = "38";
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
